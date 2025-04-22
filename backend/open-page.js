33895cadb91b130cff605f4c5d037fa0cae6db15
jabonchan <jabonchan.soap@gmail.com>
2025-04-22 16:50:57 -0400

    Use Deno's fmt command to make consistent coding style

--- backend/open-page.js
+++ backend/open-page.js
@@ -1,9 +1,9 @@
 import { child_process } from "../deps.js";
 
 export function openPage(url) {
-    const process = child_process.spawn("cmd", [ "/c", "start", url.href ]);
-    
-    return new Promise(resolve => {
+    const process = child_process.spawn("cmd", ["/c", "start", url.href]);
+
+    return new Promise((resolve) => {
         process.on("close", resolve);
-    })
+    });
 }
